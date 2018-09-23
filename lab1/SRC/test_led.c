aef4d9f6030e6a1a2a4987a46de971b5ccafef74
TheSilenceOfMind <baladkirill@mail.ru>
2018-09-23 15:44:31 +0300

    program for lab 1 finished

--- lab1/SRC/test_led.c
+++ lab1/SRC/test_led.c
@@ -28,57 +28,73 @@ e-mail: kluchev@d1.ifmo.ru
 ****************************************************************************/
 #include "aduc812.h"
 #include "led.h"
+#include "dip.h"
 
 
 // Задержка на заданное количество мс
 void delay ( unsigned long ms )
 {
-volatile unsigned long i, j;
+    volatile unsigned long i, j;
 
     for( j = 0; j < ms; j++ )
     {
         for( i = 0; i < 50; i++ );
     }
-
-
 }
+
+
+
+// update state when lightning on lamps by one
+void upd_state_on (unsigned char *animation_state)
+{
+    unsigned char state = *animation_state;
+    if (state == 0x00)
+        state = 0x81; // 1000 0001
+    else
+        state |= ((state & 0xF0) >> 1) | ((state & 0x0F) << 1);
+    *animation_state = state;
+}
+
+
+
+// update state when lightning off lamps by one
+void upd_state_off (unsigned char *animation_state)
+{
+    unsigned char state = *animation_state;
+    state = ((state & 0xF0) << 1) | ((state & 0x0F) >> 1);
+    *animation_state = state;
+}
+
+
 
 void main( void )
 {
-int i;
+    unsigned char direction = 0;    // 0 if going to led all, 1 if turn off all
+    unsigned char animation_state = 0x00;
 
-    for( i = 0; i < 3; i++ )
+    while (1)
     {
+        unsigned char dip_value = get_dip_values();
+        if (dip_value == 0x77)
+        {
+            if (direction == 0)
+            {
+                upd_state_on(&animation_state);
+                leds(animation_state);
+                if (animation_state == 0xFF)
+                    direction = 1;
+            }
+            else if (direction == 1)
+            {
+                upd_state_off(&animation_state);
+                leds(animation_state);
+                if (animation_state == 0x00)
+                    direction = 0;
+            }
+        }
+        else // if wrong dip value -> display value from DIP.
+            leds(dip_value);
 
-        leds( 0xFF );  delay( 100 );
-        leds( 0x00 );  delay( 100 );
-    }
-
-
-    led( 0, 1 ); delay( 300 );
-    led( 1, 1 ); delay( 300 );
-    led( 2, 1 ); delay( 300 );
-    led( 3, 1 ); delay( 300 );
-    led( 4, 1 ); delay( 300 );
-    led( 5, 1 ); delay( 300 );
-    led( 6, 1 ); delay( 300 );
-    led( 7, 1 ); delay( 300 );
-
-    delay( 1000 );
-
-
-    for( i = 0; i < 3; i++ )
-    {
-
-        leds( 0xFF );  delay( 100 );
-        leds( 0x00 );  delay( 100 );
-    }
-
-
-    while( 1 )
-    {
-        leds( 0x55 );  delay( 500 );
-
-        leds( 0xAA );  delay( 500 );
+        delay(500); // half of a second
     }
 }
