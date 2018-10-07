053cf43f70b3b16fcc14bb51874fd9210b7c2db8
Tatyana Ostrovskaya <ostrtanja@gmail.com>
2018-10-07 20:47:15 +0300

    add lab2 files

---
+++ lab2/INCLUDE/dip.h
@@ -0,0 +1,6 @@
+#ifndef __DIP__H
+#define __DIP__H
+
+unsigned char get_dip_values();
+
+#endif //__DIP__H
---
+++ lab2/SRC/max.c
@@ -0,0 +1,21 @@
+#include "aduc812.h"
+
+
+#define MAXBASE 0x8
+
+void write_max( unsigned char __xdata *regnum, unsigned char val ) {
+    unsigned char oldDPP = DPP;
+    DPP     = MAXBASE;
+    *regnum = val;
+    DPP     = oldDPP;   
+}
+
+unsigned char read_max( unsigned char __xdata *regnum ) {
+    unsigned char oldDPP=DPP;
+    unsigned char val;
+    DPP = MAXBASE;
+    val = *regnum;
+    DPP = oldDPP;
+    return val;
+}
+
