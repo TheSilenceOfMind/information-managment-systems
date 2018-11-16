ed6eec53413c219eec2562f3cd4030134d78d6f8
Oleg Romanov <romanov-97@mail.ru>
2018-11-16 23:25:25 +0300

    Coursework: Add main file with first mode

---
+++ coursework/src/multicooker.c
@@ -0,0 +1,72 @@
+//#include "led.h"
+//#include "max.h"
+#include "lcd.h"
+#include "keyboard.h"
+
+#define MODE_COUNT  5
+#define NEXT_SIGN '#'
+#define PREV_SIGN '*'
+#define MORE_SIGN 'A'
+#define LESS_SIGN 'B'
+
+#define MAX_PROGRAM_NUMBER 5
+
+
+void set_time( unsigned char program_number );
+void set_temperature( void );
+void cooking_process( void );
+void wish_is_cooked( void );
+
+
+void set_program( void ){
+	unsigned char msg[14] = "set program 1\0"; 
+	unsigned char program_number = 0; 
+	unsigned char symbol;
+
+	PrintStringLCD(msg);
+	for ( ;; )
+	{
+		if (0 == read_keyboard(&symbol))
+			continue;	
+
+		switch (symbol)
+		{
+			case NEXT_SIGN:
+				set_time(program_number);
+				break;
+			case MORE_SIGN:
+				program_number = (program_number + 1) % MAX_PROGRAM_NUMBER;
+				break;
+			case LESS_SIGN:
+				program_number = (MAX_PROGRAM_NUMBER + program_number - 1) % MAX_PROGRAM_NUMBER;
+				break;
+		}
+
+		msg[12] = '1' + program_number;
+		PrintStringLCD(msg);
+	}
+}
+
+void set_time( unsigned char program_number ){
+
+}
+
+void set_temperature( void ){
+
+}
+
+void cooking_process( void )
+{
+
+}
+
+void wish_is_cooked( void )
+{
+
+}
+
+void main( void )
+{
+	init_keyboard();	
+	set_program();
+}
