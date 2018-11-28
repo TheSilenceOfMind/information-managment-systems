f022a7580ce84568bc73d20087dcf3d1025eddbe
Kirill Balad <kirillbalad@gmail.com>
2018-11-28 14:27:43 +0300

    add stop after first full-play of track

--- coureswork_tests/sound/SRC/speaker.c
+++ coureswork_tests/sound/SRC/speaker.c
@@ -12,13 +12,15 @@ unsigned char cur_note_ind = 0;
 unsigned char volume = 0x1C; // максимальная громкость воспроизведения сигналов - все 4 бита в 1. 
 uint32_t cur_note_time; // время воспроизведения текущей ноты, используется как счетчик: от определенного значения до 0, после чего тональность ноты изменяется, и этот параметр снова инициализируется.
 unsigned char c = 0; // to test
+char end_of_melody = 0; // 1 - дорожка музыки закончилась, 0 - иначе.
 
 const uint16_t notes[] = {
-        392, 392, 392, 311, 466, 392, 311, 466, 392,
-		587, 587, 587, 622, 466, 369, 311, 466, 392,
-		784, 392, 392, 784, 739, 698, 659, 622, 659,
-		415, 554, 523, 493, 466, 440, 466,
-		311, 369, 311, 466, 392
+//        392, 392, 392, 311, 466, 392, 311, 466, 392,
+//		587, 587, 587, 622, 466, 369, 311, 466, 392,
+//		784, 392, 392, 784, 739, 698, 659, 622, 659,
+//		415, 554, 523, 493, 466, 440, 466,
+//		311, 369, 311, 466, 392
+200, 400, 600, 800
 };
 
 uint8_t note_delay[2];
@@ -26,7 +28,7 @@ uint8_t note_delay[2];
 // нормировочная функция для калибровки времени воспроизведения нот с разной частотой к какой-то нормали
 unsigned int get_scaled_delay(uint16_t note) {
 	float koef = note/(float)482; // 482 - среднее значение частоты, посчитанное ручками, для конкретной мелодии.
-	return (unsigned int)(300 * koef); // 300 - среднее время воспроизведения одной ноты (в единицах цикла). Находится подгонианом))0)
+	return (unsigned int)(500 * koef); // 300 - среднее время воспроизведения одной ноты (в единицах цикла). Находится подгонианом))0)
 }
 
 void set_next_note(){	
@@ -34,7 +36,9 @@ void set_next_note(){
 	cur_note_time = get_scaled_delay(notes[cur_note_ind]);
 	
 	cur_note_ind++;
-	cur_note_ind = cur_note_ind % 39; // 39 - кол-во элементов notes[]
+	cur_note_ind = cur_note_ind % 4; // 39 - кол-во элементов notes[]
+	if (cur_note_ind == 0)
+		end_of_melody = 1;
 	
 	// to test
 	++c;
@@ -49,9 +53,11 @@ void note_handler() __interrupt(5)
     TF2 = 0;                    // Like the Serial Interrupt, Timer 2 interrupt does not clear the interrupt flag that triggered the interrupt.
     EXF2 = 0;                   // Thus, be sure to always clear TF2 and EXF2 in your Timer 2 Interrupt.
 	
-    //current_ena ^= volume; // Временная отрубка: инверсия битов, отвечающих за громкость. Не спрашивайте, почему, еобходимо отрубать-врубать, чтобы был звук.
-	//write_max(ENA, current_ena);
+    current_ena ^= volume; // Временная отрубка: инверсия битов, отвечающих за громкость. Не спрашивайте, почему, еобходимо отрубать-врубать, чтобы был звук.
+	write_max(ENA, current_ena);
 	
+	if (end_of_melody == 1)
+		T2CON = 0x00; // Выключение таймера 2
 	if (--cur_note_time == 0) 
 		set_next_note();
 }
@@ -66,7 +72,8 @@ void InitSpeaker()
 {	
 	T2CON = 0x00; // Выключение таймера 2
 	
-	//set_volume(volume);
+	end_of_melody = 0;
+	set_volume(volume);
     set_next_note();
     set_vector(0x202B, (void *)note_handler);
 	
